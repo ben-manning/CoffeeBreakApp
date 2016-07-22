61beefc303a021cffb94ee69881cc829073e3478
tbduong <tiffaniduong@gmail.com>
2016-07-21 23:19:36 -0700

    set up coffeespot schema with data types

--- models/coffeespot.js
+++ models/coffeespot.js
@@ -1,2 +1,24 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+var CoffeeSpotSchema = new Schema({
+  name: String,
+  address: String,
+  _location: [Location.schema],
+  freeWifi: Boolean,
+  fastWifi: Boolean,
+  outlets: Boolean,
+  openNow: Boolean,
+  goodCoffee: Boolean,
+  lively: Boolean,
+  quiet: Boolean,
+  goodForGroups: Boolean,
+  outdoorSeating: Boolean,
+  petFriendly: Boolean,
+  parkingLot: Boolean,
+  //recommendations
+});
+
+var CoffeeSpot = mongoose.model('CoffeeSpot', CoffeeSpotSchema);
+
+module.exports = CoffeeSpot;
