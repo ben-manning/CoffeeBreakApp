fec6622d6e53e3866afac008658d3529271493d8
tbduong <tiffaniduong@gmail.com>
2016-07-23 15:40:25 -0700

    SUCCESS able to find location by id-- function show

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -9,6 +9,16 @@ function index(req, res) {
     });
 }
 
+//find location by id
+function show (req, res) {
+  var locationId = req.params.location_id;
+  db.Location.findById(locationId, function(err, foundLocation){
+      if (err) {console.log("!! ERROR COULD NOT FIND LOCATION");}
+      console.log("SUCCESS! Found location:" + foundLocation);
+      res.json(foundLocation);
+  });
+}
+
 
 
 
@@ -32,4 +42,5 @@ function index(req, res) {
 
 module.exports = {
   index: index,
+  show: show
 };
